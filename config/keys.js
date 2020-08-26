87758ece8e18c96ee10585f785ecba41688d574d
Bhavykapadiya <kapadiyabhavy@gmail.com>
2020-08-26 18:15:35 +0530

    Initial Commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://mongo:mongo@cluster0.v30pz.mongodb.net/passport?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://baap:baap@cluster0.v30pz.mongodb.net/NodePassport?retryWrites=true&w=majority'
 }
